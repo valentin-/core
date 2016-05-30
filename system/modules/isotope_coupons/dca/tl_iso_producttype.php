ce4a1fc69c959548aef27484fc7c5bc09f439dec
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-05-30 16:44:13 +0200

    Finalize backend config view

--- system/modules/isotope_coupons/dca/tl_iso_producttype.php
+++ system/modules/isotope_coupons/dca/tl_iso_producttype.php
@@ -14,30 +14,76 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_iso_producttype']['palettes']['coupon'] = '{name_legend},name,class,fallback;{description_legend:hide},description;{prices_legend:hide},prices;{template_legend},list_template,reader_template,list_gallery,reader_gallery;{attributes_legend},attributes;{coupon_legend},coupon_prefix,coupon_length,coupon_chars';
+$GLOBALS['TL_DCA']['tl_iso_producttype']['palettes']['coupon'] = '{name_legend},name,class,fallback;{description_legend:hide},description;{prices_legend:hide},prices;{template_legend},list_template,reader_template,list_gallery,reader_gallery;{attributes_legend},attributes;{coupon_legend},coupon_info,coupon_prefix,coupon_numbers,coupon_alphabet,coupon_chars';
 
 
 /**
  * Fields
  */
+$GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['coupon_info']['input_field_callback'] = function(\DataContainer $dc) {
+    if ($dc->activeRecord->coupon_chars < 1
+        || $dc->activeRecord->coupon_chars > strlen($dc->activeRecord->coupon_alphabet)
+    ) {
+        return '';
+    }
+
+    $alphabet = str_split($dc->activeRecord->coupon_alphabet);
+    $code = '';
+
+    if ($dc->activeRecord->coupon_prefix) {
+        $code = $dc->activeRecord->coupon_prefix . '-';
+    }
+
+    $code .= str_pad('1', $dc->activeRecord->coupon_numbers, '0', STR_PAD_LEFT) . '-';
+
+    $code .= implode(
+        '',
+        array_intersect_key(
+            $alphabet,
+            array_flip(
+                array_rand(
+                    $alphabet,
+                    $dc->activeRecord->coupon_chars
+                )
+            )
+        )
+    );
+
+    return '<p class="tl_info" style="margin-top:10px">' . sprintf($GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_info'], $code) . '</p>';
+};
+
 $GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['coupon_prefix'] = [
-    'label'         => $GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_prefix'],
+    'label'         => &$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_prefix'],
     'exclude'       => true,
     'inputType'     => 'text',
     'eval'          => ['maxlength' => 64, 'tl_class' => 'w50'],
+    'sql'           => "varchar(64) NOT NULL default ''",
 ];
 
-$GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['coupon_length'] = [
-    'label'         => $GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_length'],
+$GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['coupon_numbers'] = [
+    'label'         => &$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_numbers'],
     'exclude'       => true,
+    'default'       => 4,
+    'inputType'     => 'select',
+    'options'       => [1,2,3,4,5,6,7,8,9],
+    'eval'          => ['mandatory' => true, 'tl_class' => 'w50'],
+    'sql'           => "char(1) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['coupon_alphabet'] = [
+    'label'         => &$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_alphabet'],
+    'exclude'       => true,
+    'default'       => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
     'inputType'     => 'text',
-    'eval'          => ['mandatory' => true, 'rgxp' => 'prcnt', 'maxlength' => 2, 'tl_class' => 'w50'],
+    'eval'          => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+    'sql'           => "varchar(255) NOT NULL default ''",
 ];
 
 $GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['coupon_chars'] = [
-    'label'         => $GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_chars'],
+    'label'         => &$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_chars'],
     'exclude'       => true,
-    'default'       => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-',
+    'default'       => 8,
     'inputType'     => 'text',
-    'eval'          => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'clr long'],
+    'eval'          => ['mandatory' => true, 'rgxp' => 'prcnt', 'maxlength' => 2, 'tl_class' => 'w50'],
+    'sql'           => "varchar(2) NOT NULL default ''",
 ];
---
+++ system/modules/isotope_coupons/languages/en/tl_iso_producttype.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_prefix'] = ['Prefix', 'You can optionally add a prefix to the coupon.'];
+$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_numbers'] = ['Coupon number length', 'Select the minimum length of the coupon number (not including coupon prefix).'];
+$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_alphabet']  = ['Allowed characters', 'Enter all characters that are allowed in the coupon code (case sensitive).'];
+$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_chars'] = ['Code length', 'Enter the length of the unique coupon code.'];
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_legend'] = 'Coupon Configuration';
+$GLOBALS['TL_LANG']['tl_iso_producttype']['coupon_info']   = 'A coupon code is a dash-delimited combination of prefix, code number and several unique characters.<br>Live example (save to refresh): %s';
