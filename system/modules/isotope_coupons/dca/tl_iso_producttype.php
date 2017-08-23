5ae7916f093926a94616ecdcb462b09d4ce8d0d6
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-08-23 09:39:13 +0200

    Added variants and other default fields to product type

--- system/modules/isotope_coupons/dca/tl_iso_producttype.php
+++ system/modules/isotope_coupons/dca/tl_iso_producttype.php
@@ -14,7 +14,7 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_iso_producttype']['palettes']['coupon'] = '{name_legend},name,class,fallback;{description_legend:hide},description;{prices_legend:hide},prices;{template_legend},list_template,reader_template,list_gallery,reader_gallery;{attributes_legend},attributes;{coupon_legend},coupon_info,coupon_prefix,coupon_numbers,coupon_alphabet,coupon_chars';
+$GLOBALS['TL_DCA']['tl_iso_producttype']['palettes']['coupon'] = '{name_legend},name,class,fallback;{description_legend:hide},description;{prices_legend:hide},prices;{template_legend},list_template,reader_template,list_gallery,reader_gallery,cssClass;{attributes_legend},attributes;{variants_legend:hide},variants;{expert_legend:hide},shipping_exempt,downloads;{coupon_legend},coupon_info,coupon_prefix,coupon_numbers,coupon_alphabet,coupon_chars';
 
 
 /**
